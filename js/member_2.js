5eba4ef14859cbe5b92381bcee3c8828cf244886
WIN-NGPQIU1L05J\Administrator <fulgidg@gmail.com>
2017-01-17 01:43:07 +0900

    -

--- js/member_2.js
+++ js/member_2.js
@@ -1,22 +1,47 @@
-$.fn.schAgree = function(){
-	var agree = $("#s_agree_y");
-	var agree1 = $("#s_agree_y_1");
+/*popup_인증*/
+$.fn.certi = function(){
 	var ts = $(this);
+	var ts1 = ts.eq(0);
+	var ts2 = ts.eq(1);
+	var el_1 = $("<div/>",{"class":"certifi_dim"});
+	ts1.on("click",function(){
+		$(".certifi_phone").fadeIn(500);
+		$("body").append(el_1);
+	});
+	ts2.on("click",function(){
+		$(".certifi_email").fadeIn(500);
+		$("body").append(el_1);
+	});
+};
+
+/*popup_닫기버튼*/
+$.fn.certi1 = function(){
+	var ts = $(this);
+	var btnClose = $(".btn_close");
+	var btnReset = $("input[type='reset']");
+	var getNum = $("input[name='phone_3']").next();
+
+	getNum.on("click",function(){
+		$(this).closest("p").next().show();
+	});
+	btnClose.on("click",function(){
+		$(this).parent().fadeOut(500);
+		$(".certifi_dim").remove();
+		getNum.closest("p").next().hide();
+	});
+	btnReset.on("click",function(){
+		ts.fadeOut(500);
+		$(".certifi_dim").remove();
+		getNum.closest("p").next().hide();
+	});
+	
 	ts.on("submit",function(){
-		if(!agree.is(":checked")){
-			alert("이용약관에 동의하지 않으셨습니다. 이용약관에 동의하셔야 가입할 수 있습니다.");
-			$("#s_agree_y").focus();
-			return false;
-		};
-		if(!agree1.is(":checked")){
-			alert("개인정보 취급방침에 동의하지 않으셨습니다. 이용약관에 동의하셔야 가입할 수 있습니다.");
-			$("#s_agree_y_1").focus();
-			return false;
-		};
+
 	});
 };
 
 $(function(){
-	$(".member_agree").schAgree();
+	$(".certifi section a").certi();
+	$(".certifi_phone,.certifi_email").certi1();
 });
 
