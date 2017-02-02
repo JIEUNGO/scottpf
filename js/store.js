a39e0dbefcfef21bf26286ad758bd2cb10871ba8
WIN-NGPQIU1L05J\Administrator <fulgidg@gmail.com>
2017-02-02 12:08:07 +0900

    -

--- js/store.js
+++ js/store.js
@@ -40,12 +40,11 @@
 			e.preventDefault();
 			var url = $(this).attr("href");
 			$.get(url,function(data){
-				//console.log(data);
 				var el_1 = $("<div/>",{"class":"pop_dim"});
 				var el_2 = $("<div/>",{"class":"pop_inner_wrap"}).appendTo(el_1);
 				var el_3 = $("<div/>",{"class":"pop_inner"}).appendTo(el_2);
 				$("<button/>",{"class":"btnCloseLayer","text":"닫기"}).appendTo(el_3);
-				el_3.prepend(data); //preappend- 앞으로 들어감
+				el_3.prepend(data);
 				$("body").append(el_1);
 			});
 			$(document).on("click",".btnCloseLayer",function(){
